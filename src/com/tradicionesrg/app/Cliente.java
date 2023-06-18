e4a95db872c9ddc2b5254ca791f01d0377357215
gpauletti <gastonpauletti@gmail.com>
2023-06-18 00:23:19 -0300

    TradicionesRg#2

---
+++ src/com/tradicionesrg/app/Cliente.java
@@ -0,0 +1,14 @@
+package com.tradicionesrg.app;
+
+public class Cliente {
+    private int id;
+    private String name;
+    private String surname;
+    private int dni;
+    private int cuil;
+    private int cuit;
+    private String address;
+    private int phone;
+    private String email;
+
+}
---
+++ src/com/tradicionesrg/app/Factura.java
@@ -0,0 +1,12 @@
+package com.tradicionesrg.app;
+import com.tradicionesrg.app.*;
+
+public class Factura {
+
+    public int number_order;
+    public int[] items;
+
+
+
+
+}
